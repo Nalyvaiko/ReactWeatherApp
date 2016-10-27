7829fbfee015ea3c590ed7850612c81fc0a6b486
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-10-27 19:30:32 +0300

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ var express = require('express');
 // Create app
 var app = express();
 
+// If running from Heroku - use env.port, else 3000
+const PORT = process.env.PORT || 3000;
+
+//express middleware, check if traffic http || https - redirect
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 // which folder to serve
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-    console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT);
 })
