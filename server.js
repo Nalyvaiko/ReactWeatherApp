010cda56061564429c43015e94d70be427410019
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-10-27 20:44:10 +0300

    Update middle ware for localhost

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var app = express();
 // If running from Heroku - use env.port, else 3000
 const PORT = process.env.PORT || 3000;
 
-//express middleware, check if traffic http || https - redirect
+//express middleware, redirect traffic from https to http
 app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
+    if (req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
     }
 });
 
